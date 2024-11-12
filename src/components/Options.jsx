7c4e5b07f165dfdd300403da6819523165e738cb
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2024-11-12 19:03:12 +0000

    added star component

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,14 +1,12 @@
+import Star from './Star'
 function Options({ question, dispatch, answer }) {
   const hasAnswered = answer !== null;
   question["options"]=question["optString"].split("/");
+  let diffDic={"easy":<Star/>,"medium":<><Star/><Star/></>,"hard":<><Star/><Star/><Star/></>}
       let r=question["options"][question.correctOption];
   let s=question["options"][answer];
-  return (
-    <div>
-      {s}
-      <br/>
-      {r}<br/>
-      {question.difficulty}<br/>
+  return (<div>
+<div className="flex m-4">Difficulty: &nbsp;&nbsp;{diffDic[question["difficulty"]]}</div>
     <div className="flex flex-col">
     {question.options.map((option, index) => (
         <button style={{border:"1px solid darkred",borderRadius:"12px"}}
