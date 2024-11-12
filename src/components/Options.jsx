d72590e1ffb6af7f1dcb5c1de4ae7c40075f2a70
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2024-11-12 16:49:48 +0000

    minor fearure

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -12,13 +12,13 @@ function Options({ question, dispatch, answer }) {
     <div className="flex flex-col">
     {question.options.map((option, index) => (
         <button style={{border:"1px solid darkred",borderRadius:"12px"}}
-          className={`m-4 btn btn-option ${index === answer ? r==s?"answerc" : "answerw":""} ${
+          className={`m-4 btn btn-option  ${
             hasAnswered
-              ? index === question.correctOption
-                ? "answerc"
+              ? index === question.correctOption && r!=s
+                ? "answerd"
                 : "wrong"
               : ""
-          }`}
+          } ${index === answer ? r==s?"answerc" : "answerw":""}`}
           key={index}
           disabled={hasAnswered}
           onClick={() => dispatch({ type: "newAnswer", payload: index })}
