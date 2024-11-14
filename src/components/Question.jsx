b10ea340c00d9ae150ac3cde5ef6cbad27d375f9
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2024-11-14 20:36:37 +0000

    c

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -2,8 +2,8 @@ import Options from "./Options";
 
 function Question({ question, dispatch, answer }) {
   return (
-    <div className="question_container">
-      <h2 className="text-center mt-6 mb-4">{question.question}</h2>
+    <div className="question-container">
+      <div className="quest-size text-center mt-6 mb-4">{question.question}</div>
       <Options question={question} dispatch={dispatch} answer={answer} />
     </div>
   );
