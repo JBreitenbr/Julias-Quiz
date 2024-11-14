0beba472a9027783fe0a5fb628d8968d980483a2
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2024-11-14 20:07:32 +0000

    adding some more styles

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,12 +1,13 @@
+import QuestionMark from "./QuestionMark";
 function StartScreen({dispatch, cat, handleCat,numQuestions, handleNum}) {
 let cats=["Literature","Philosophy","Biology","History","Geography","Movies","Music","Linguistics","Computer Science","Mathematics"];
 let nums=[5,10,15,20];                                  
   return (
     <div className="start flex flex-col items-center">
-      <h1 className="m-4">Julias Super Quiz</h1>
+      <h1 className="mt-6">Julias Super Quiz</h1><QuestionMark/>
       <h2>Have lots of fun!</h2>
-      <select value={cat} onChange={handleCat} className="text-slate-800 p-4 m-6 rounded-3xl"><option>Select a category</option>{cats.map(item=><option  value={item}>{item}</option>)}</select><br/><div className="flex"><p>Number of questions:&nbsp;&nbsp;</p>
-      <select value={numQuestions} onChange={handleNum} className="text-slate-800 p-2 rounded-3xl">{nums.map(item=><option value={item}>{item}</option>)}</select></div><br/>
+      <select value={cat} onChange={handleCat} className="text-slate-800 p-4 m-6 rounded-3xl"><option>Select a category</option>{cats.map(item=><option  value={item} key={item}>{item}</option>)}</select><br/><div className="flex"><p className="quest-num">Number of questions:&nbsp;&nbsp;</p>
+      <select value={numQuestions} onChange={handleNum} className="text-slate-800 p-2 rounded-3xl">{nums.map(item=><option value={item} key={item}>{item}</option>)}</select></div><br/>
       <button
         className="btn btn-ui" onClick={() => dispatch({ type: "start" })}
       >
