3551f86e7686efda85d04f2cdcb744165c38c6a6
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2024-11-17 07:18:30 +0000

    react respond

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,7 +2,7 @@ export default function Loader() {
   return (
     <div className="flex flex-col items-center justify-items-center">
       <div className="loader"></div>
-      <p>Loading questions...</p>
+      <p className="text-2xl sm:text-3xl">Loading questions...</p>
     </div>
   );
 }
